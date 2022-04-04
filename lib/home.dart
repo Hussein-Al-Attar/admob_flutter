068c1b28c6aa0e0a3a350a490023bd23e3292721
96478 <HUSSEIN2828>
2022-04-04 21:51:50 +0300

    bannnerAd

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:admob_flutter/admob/banner_ad_model.dart';
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return adBanner();
+  }
+}
